bfad9d4256f8014589cd17b17d918bdb2c01dd5a
dridrileite <adrianacristinasouza@gmail.com>
2023-05-24 19:48:02 -0300

    inserção cors: teste localhost

--- src/api-bma/api-bma/Program.cs
+++ src/api-bma/api-bma/Program.cs
@@ -3,7 +3,21 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Text.Json.Serialization;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+            
+        });
+});
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -36,6 +50,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
--- src/bma-front/src/components/layouts/form/TrackNis.js
+++ src/bma-front/src/components/layouts/form/TrackNis.js
@@ -2,51 +2,76 @@ import React, { useEffect, useState } from 'react';
 import Input from './Input';
 
 
-function TrackNis() {
+
+function TrackNis({beneficiariosData}) {
     const [name,setName]=useState([])
-    
-    function onBlurNis (e) {
-        const nis =(e) =>{
-            e.target.value; 
-        }
+    const [beneficiarios,setBeneficiarios]=useState(beneficiariosData || {});
+
+    const convertToArray=(obj)=>{
+        const arr =[obj]
+        return arr
+    }
+    const onBlurNis = (e) => {
+        e.preventDefault()
+        const {value} = e.target
+        const nis =value.replace(/[^0-9]/g,'')
+        
         
         fetch(`https://localhost:7255/${nis}`, {
             method: 'GET',
             headers:{
                 'Content-Type':'aplication/json',
+                withCredentials: true,    
+                crossorigin: true,
+                mode: 'no-cors', 
 
             },
             
         })           
             .then((resp) => resp.json())
             .then((data)=> {
+                const array=convertToArray(data);
+                setName(array)
                 
-                setName(data)
             })
             .catch((err) => console.log(err))
             
         
     }
+     function handleChange(e) {
+        setBeneficiarios({...beneficiarios, [e.target.name]: e.target.value })             
+    }
+    
     return(
       <>
         <div>
-        {name.length > 0 ? (name.map ((item) =>(
+        
+        <Input
+                                   
+            type="text" 
+            text="NIS"
+            name="nis"                                              
+            onBlur={onBlurNis}
+            onChange={handleChange}                
+        />
+        {name.length > 0 ? (name.map ((name) =>(        
+        
             <Input
-                key={item.Id}                   
-                type="text" 
-                text="NIS"
-                name="nis"
-                value={item.id}                               
-                onBlur={onBlurNis}                
-            />
-                
-                    ))):(
-            <Input
+                key={name.nome}
                 type="text" 
                 text="Nome"
                 name="nome"
+                value={beneficiarios.nome}
+                onChange={handleChange}
                 placeholder=""                                
-        />)}
+        />))):(
+            <Input
+                                    
+           type="text" 
+           text="Nome"
+           name="nome"
+           placeholder=""                              
+           /> )} 
         </div>
         <div>
         <button>Buscar</button>                                                             
